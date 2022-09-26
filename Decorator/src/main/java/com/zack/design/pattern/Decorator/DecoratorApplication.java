a4fd487bec734bc7005d3b3dc44a0ef138c3112b
Nguyễn Minh Tuấn <59287970+prostatus123@users.noreply.github.com>
2022-09-26 09:33:32 +0700

    create Design_Pattern_Decorator_pattern

---
+++ Decorator/src/main/java/com/zack/design/pattern/Decorator/DecoratorApplication.java
@@ -0,0 +1,13 @@
+package com.zack.design.pattern.Decorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoratorApplication.class, args);
+	}
+
+}
